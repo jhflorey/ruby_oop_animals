506968e5eddf22a2b88a8a90456b64eed1811394
Jessica Florey <jhflorey@Jessicas-MacBook-Pro-2.local>
2017-03-04 20:00:05 -0800

    first commit

---
+++ mammal.rb
@@ -0,0 +1,14 @@
+puts 'I am in the mammal file'
+
+class Mammal
+	attr_accessor :alive
+	def initialize
+		@alive = true
+		puts "I am alive!"
+		self
+	end
+	def breathe
+		puts "Inhale and exhale"
+		self
+	end
+end
